951c373c33fe2356cc1228d8a96f0325f9514a0b
Sunngine <120574672+Sunngine@users.noreply.github.com>
2022-12-28 17:12:24 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+// let name = 'Артём';
+// console.log (`Привет, ` + name + `!` );
+// console.log (`Привет, ${name}!`);
+// let firstoperand = 3;
+// let secondoperand = 5;
+
+//console.log(`${firstoperand} + ${secondoperand} = ${firstoperand + secondoperand}`);
+
+// let a = 10, b = 5;
+// console.log(a+b); // 15
+// console.log(a-b); // 5
+// console.log(a*b); // 50
+// console.log(a/b); // 2
+// совмещаем
+// let c = a + b;
+
+//let a = 26, b = 13;
+//console.log(`${a} + ${b} = ${a + b}`);
+//console.log(`${a} - ${b} = ${a - b}`);
+//console.log(`${a} * ${b} = ${a * b}`);
+//console.log(`${a} / ${b} = ${a / b}`);
+
+// let a = 24; 
+// let b = 6;
+
+// let sum = a + b;
+// let difference = a - b;
+// let multiplication = a * b;
+// let division = a / b;
+
+
+// console.log(`${a} + ${b} = ${sum}`);
+// console.log(`${a} - ${b} = ${difference}`);
+// console.log(`${a} * ${b} = ${multiplication}`);
+// console.log(`${a} / ${b} = ${division}`);
+
+let userName = `myname`;
+let bonusBalance = 1000;
+
+console.log(`Пользователь - ` + userName);
+console.log(`bonusBalance = ` + bonusBalance);
+
+
+// addBalance - добавление баллов при покупке
+let addBalance = 50;
+// burnBalance - сжигаемые баллы
+let burnBalance = 3;
+// total - итоговый подсчет баллов
+let multiplication1 = addBalance * 3;
+let multiplication2 = burnBalance * 7
+let total = bonusBalance + multiplication1 - multiplication2
+console.log(`Пользователь - ` + userName);
+console.log(`bonusBalance = ` + total);
+
+// addBalance - добавление баллов при покупке
+// let addBalance = 50;
+// burnBalance - сжигаемые баллы
+// let burnBalance = 3;
+console.log(`bonusBalance =` + `${bonusBalance} + ${addBalance*3} - ${burnBalance*7}`);
+
+// = {a*3}-{b*7}
